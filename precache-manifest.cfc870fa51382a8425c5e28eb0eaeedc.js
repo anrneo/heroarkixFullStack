31a7c74e66cd161adc06739bb179180ecb85c4a0
anrneo <anreno@gmail.com>
2020-08-12 12:38:57 -0500

    first

---
+++ precache-manifest.cfc870fa51382a8425c5e28eb0eaeedc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8f38aa5d2c784b047c830d6029575e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbed4fbf1d0a3a9a4aff",
+    "url": "/static/css/2.118dfa7b.chunk.css"
+  },
+  {
+    "revision": "2abb58b8f9564d1111e0",
+    "url": "/static/css/main.af79ed23.chunk.css"
+  },
+  {
+    "revision": "fbed4fbf1d0a3a9a4aff",
+    "url": "/static/js/2.d8216395.chunk.js"
+  },
+  {
+    "revision": "2abb58b8f9564d1111e0",
+    "url": "/static/js/main.96ea07c9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
